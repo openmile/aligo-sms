21e8f100932fdb558de7b57586a85e408de85e07
gyeongjung <monom.kim@gmail.com>
2021-05-03 17:21:15 +0900

    update requirements and refactor some codes

--- app/aligo.py
+++ app/aligo.py
@@ -1,11 +1,14 @@
 import aiohttp
+import asyncio
 
-ALIGO_URL = 'https://apis.aligo.in'
+ALIGO_URL = "https://apis.aligo.in"
 
 
-async def aligo_sms(data):
-    async with aiohttp.ClientSession() as session:
-        aligo_suffix = '/send/'
-        async with session.post(ALIGO_URL + aligo_suffix, data=data, ssl=False) as response:
-            aligo_body = await response.json(content_type=None)
-            return aligo_body
+async def aligo_message(session: aiohttp.ClientSession, aligo_data, url_suffix=None):
+    aligo_suffix = url_suffix or "/send/"
+    async with session.post(
+        ALIGO_URL + aligo_suffix,
+        data=aligo_data,
+        ssl=False,
+    ) as response:
+        return await response.json(content_type=None)
